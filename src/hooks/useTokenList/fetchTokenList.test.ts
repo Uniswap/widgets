4feeee3856455aa4dfadb5f8908232dc927afef5
Zach Pomerantz <zzmp@uniswap.org>
2022-05-02 14:20:12 -0700

    test: update fetchTokenList test

--- src/hooks/useTokenList/fetchTokenList.test.ts
+++ src/hooks/useTokenList/fetchTokenList.test.ts
@@ -1,10 +1,16 @@
-import fetchTokenList, { DEFAULT_TOKEN_LIST } from './fetchTokenList'
+import defaultTokenList from '@uniswap/default-token-list'
+
+import fetchTokenList from './fetchTokenList'
 
 describe('fetchTokenList', () => {
   const resolver = jest.fn()
+  beforeEach(() => resolver.mockReset())
 
   it('throws on an invalid list url', async () => {
-    const url = 'https://example.com'
+    const url = 'https://example.com/invalid-tokenlist.json'
+    fetchMock.mockIf(url, () => {
+      throw new Error()
+    })
     await expect(fetchTokenList(url, resolver)).rejects.toThrowError(`failed to fetch list: ${url}`)
     expect(resolver).not.toHaveBeenCalled()
   })
@@ -13,13 +19,16 @@ describe('fetchTokenList', () => {
     const url = 'example.eth'
     const contenthash = '0xD3ADB33F'
     resolver.mockResolvedValue(contenthash)
-    await expect(fetchTokenList(url, resolver)).rejects.toThrow()
+    await expect(fetchTokenList(url, resolver)).rejects.toThrow(
+      `failed to translate contenthash to URI: ${contenthash}`
+    )
     expect(resolver).toHaveBeenCalledWith(url)
   })
 
   it('fetches and validates the default token list', async () => {
-    const list = await (await fetch(DEFAULT_TOKEN_LIST)).json()
-    await expect(fetchTokenList(DEFAULT_TOKEN_LIST, resolver)).resolves.toStrictEqual(list)
+    const url = 'https://example.com/default-tokenlist.json'
+    fetchMock.mockIf(url, () => Promise.resolve(JSON.stringify(defaultTokenList)))
+    await expect(fetchTokenList(url, resolver)).resolves.toStrictEqual(defaultTokenList)
     expect(resolver).not.toHaveBeenCalled()
   })
 })
--- src/hooks/useTokenList/fetchTokenList.ts
+++ src/hooks/useTokenList/fetchTokenList.ts
@@ -5,8 +5,6 @@ import uriToHttp from 'utils/uriToHttp'
 
 import validateTokenList from './validateTokenList'
 
-export const DEFAULT_TOKEN_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
-
 const listCache = new Map<string, TokenList>()
 
 /** Fetches and validates a token list. */
