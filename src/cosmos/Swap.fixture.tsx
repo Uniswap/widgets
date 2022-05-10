1e2fe39f54955a3942339d18a2735bfb5ec4bdaa
Zach Pomerantz <zzmp@uniswap.org>
2022-05-10 14:50:18 -0700

    test: simplify cosmos token lists

--- src/cosmos/Swap.fixture.tsx
+++ src/cosmos/Swap.fixture.tsx
@@ -1,12 +1,18 @@
 import { tokens } from '@uniswap/default-token-list'
 import { TokenInfo } from '@uniswap/token-lists'
-import { darkTheme, DEFAULT_LOCALE, defaultTheme, lightTheme, SUPPORTED_LOCALES, SwapWidget } from '@uniswap/widgets'
+import {
+  darkTheme,
+  DEFAULT_LOCALE,
+  defaultTheme,
+  lightTheme,
+  SUPPORTED_LOCALES,
+  SupportedChainId,
+  SwapWidget,
+} from '@uniswap/widgets'
 import { useEffect } from 'react'
-import { useSelect, useValue } from 'react-cosmos/fixture'
+import { useValue } from 'react-cosmos/fixture'
 
-import { SupportedChainId } from '../constants/chains'
 import { DAI, USDC_MAINNET } from '../constants/tokens'
-import { TokenListProvider } from '../hooks/useTokenList'
 import useJsonRpcEndpoint from './useJsonRpcEndpoint'
 import useOption from './useOption'
 import useProvider from './useProvider'
@@ -43,52 +49,30 @@ function Fixture() {
 
   const jsonRpcEndpoint = useJsonRpcEndpoint()
   const connector = useProvider()
-  const tokenListNameMap: Record<string, TokenInfo[] | string> = {
-    'default list': tokens,
-    'mainnet only': tokens.filter((token) => SupportedChainId.MAINNET === token.chainId),
-    'arbitrum only': [
-      {
-        logoURI: 'https://assets.coingecko.com/coins/images/9956/thumb/4943.png?1636636734',
-        chainId: 42161,
-        address: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
-        name: 'Dai Stablecoin',
-        symbol: 'DAI',
-        decimals: 18,
-      },
-      {
-        logoURI: 'https://assets.coingecko.com/coins/images/6319/thumb/USD_Coin_icon.png?1547042389',
-        chainId: 42161,
-        address: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-        name: 'USD Coin (Arb1)',
-        symbol: 'USDC',
-        decimals: 6,
-      },
-    ],
-  }
 
-  const tokenListOptions = Object.keys(tokenListNameMap)
-  const [tokenListName] = useSelect('tokenList', {
-    options: tokenListOptions,
-    defaultValue: tokenListOptions[0],
-  })
+  const tokenLists: Record<string, TokenInfo[]> = {
+    Default: tokens,
+    'Mainnet only': tokens.filter((token) => token.chainId === SupportedChainId.MAINNET),
+  }
+  const tokenList = useOption('tokenList', { options: tokenLists, defaultValue: 'Default', nullable: false })
+  console.log(tokenList)
+
   return (
-    <TokenListProvider list={tokenListNameMap[tokenListName]}>
-      <SwapWidget
-        convenienceFee={convenienceFee}
-        convenienceFeeRecipient={convenienceFeeRecipient}
-        defaultInputTokenAddress={defaultInputToken}
-        defaultInputAmount={defaultInputAmount}
-        defaultOutputTokenAddress={defaultOutputToken}
-        defaultOutputAmount={defaultOutputAmount}
-        locale={locale}
-        jsonRpcEndpoint={jsonRpcEndpoint}
-        provider={connector}
-        theme={theme}
-        tokenList={tokens}
-        width={width}
-        onConnectWallet={() => console.log('onConnectWallet')} // this handler is included as a test of functionality, but only logs
-      />
-    </TokenListProvider>
+    <SwapWidget
+      convenienceFee={convenienceFee}
+      convenienceFeeRecipient={convenienceFeeRecipient}
+      defaultInputTokenAddress={defaultInputToken}
+      defaultInputAmount={defaultInputAmount}
+      defaultOutputTokenAddress={defaultOutputToken}
+      defaultOutputAmount={defaultOutputAmount}
+      locale={locale}
+      jsonRpcEndpoint={jsonRpcEndpoint}
+      provider={connector}
+      theme={theme}
+      tokenList={tokenList}
+      width={width}
+      onConnectWallet={() => console.log('onConnectWallet')} // this handler is included as a test of functionality, but only logs
+    />
   )
 }
 
