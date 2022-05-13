f66536fc9381dbbdd0513a27e7cdc6cdea272a8c
Zach Pomerantz <zzmp@uniswap.org>
2022-05-13 16:23:49 -0700

    test: mv test out of src

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  setupFiles: ['<rootDir>/src/tests/setup.ts'],
+  setupFiles: ['<rootDir>/test/setup.ts'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '.scss$': '<rootDir>/src/tests/scssStub',
-    '.(png|svg)$': '<rootDir>/src/tests/imageStub',
+    '.scss$': '<rootDir>/test/scssStub',
+    '.(png|svg)$': '<rootDir>/test/imageStub',
   },
 }
