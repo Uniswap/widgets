fa209d1354be11e813f46942e2edf2ecf0197e0c
Kristie Huang <kristiehuang01@gmail.com>
2022-06-16 12:14:37 -0400

    Connect eagerly on mount

--- src/components/ConnectWallet/index.tsx
+++ src/components/ConnectWallet/index.tsx
@@ -1,4 +1,6 @@
 import Wallet from 'components/Wallet'
+import { connections } from 'hooks/connectWeb3/useConnect'
+import { useEffect } from 'react'
 
 import ConnectedWalletChip from './ConnectedWalletChip'
 
@@ -9,6 +11,16 @@ interface ConnectWalletProps {
 }
 
 export default function ConnectWallet({ account, shouldOpenIntegratorFlow, onConnectWallet }: ConnectWalletProps) {
+  // Attempt to connect eagerly on mount
+  useEffect(() => {
+    connections.forEach(
+      ([wallet, _]) =>
+        void wallet.connectEagerly().catch(() => {
+          console.debug('Failed to connect eagerly to wallet')
+        })
+    )
+  }, [])
+
   if (Boolean(account)) {
     return <ConnectedWalletChip account={account} />
   } else {
