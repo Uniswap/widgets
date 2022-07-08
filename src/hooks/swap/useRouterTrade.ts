eaae27f92840885c8631e931aeda4f54cb2d962f
Kristie Huang <kristiehuang01@gmail.com>
2022-07-08 13:25:22 -0400

    remove jank logic

--- src/hooks/swap/useRouterTrade.ts
+++ src/hooks/swap/useRouterTrade.ts
@@ -3,7 +3,6 @@ import useAutoRouterSupported from 'hooks/routing/useAutoRouterSupported'
 import { useRoutingAPITrade } from 'hooks/routing/useRoutingAPITrade'
 import useDebounce from 'hooks/useDebounce'
 import useIsWindowVisible from 'hooks/useIsWindowVisible'
-import useLast from 'hooks/useLast'
 import { useMemo } from 'react'
 import { InterfaceTrade, TradeState } from 'state/routing/types'
 
@@ -32,34 +31,10 @@ export function useRouterTrade(
     200
   )
 
-  const tradeObject = useRoutingAPITrade(
+  return useRoutingAPITrade(
     tradeType,
     routerApiBaseUrl,
     autoRouterSupported && isWindowVisible ? debouncedAmount : undefined,
     debouncedOtherCurrency
   )
-
-  const lastTrade = useLast(tradeObject.trade, Boolean) ?? undefined
-
-  // Return the last trade while syncing/loading to avoid jank from clearing the last trade while loading.
-  // If the trade is unsettled and not stale, return the last trade as a placeholder during settling.
-  return useMemo(() => {
-    const { state, trade } = tradeObject
-    // If the trade is in a settled state, return it.
-    if (state === TradeState.INVALID) return INVALID_TRADE
-    if ((state !== TradeState.LOADING && state !== TradeState.SYNCING) || trade) return tradeObject
-
-    const [currencyIn, currencyOut] =
-      tradeType === TradeType.EXACT_INPUT
-        ? [amountSpecified?.currency, otherCurrency]
-        : [otherCurrency, amountSpecified?.currency]
-
-    // If the trade currencies have switched, consider it stale - do not return the last trade.
-    const isStale =
-      (currencyIn && !lastTrade?.inputAmount?.currency.equals(currencyIn)) ||
-      (currencyOut && !lastTrade?.outputAmount?.currency.equals(currencyOut))
-    if (isStale) return tradeObject
-
-    return { state, trade: lastTrade }
-  }, [amountSpecified?.currency, lastTrade, otherCurrency, tradeObject, tradeType])
 }
