493dabf26f2dcb67311f5469d26185199dbb7866
Kristie Huang <kristiehuang01@gmail.com>
2022-07-20 14:35:20 -0400

    feat: add deactivate fxn

--- src/components/ConnectWallet/ConnectedWalletChip.tsx
+++ src/components/ConnectWallet/ConnectedWalletChip.tsx
@@ -7,13 +7,14 @@ import Identicon from 'icons/identicon'
 import styled from 'styled-components/macro'
 import { ThemedText } from 'theme'
 import { useState } from 'react'
+import { connections } from 'hooks/connectWeb3/useWeb3React'
+import { Network } from '@web3-react/network'
+import { Url } from '@web3-react/url'
 
 const AccountButton = styled(TextButton)<{ hidden?: boolean }>`
   filter: none;
   visibility: ${({ hidden }) => hidden && 'hidden'};
 `
-
-const AccountButtonText = styled(ThemedText.Subhead2)``
 
 export default function ConnectedWalletChip({ disabled }: { disabled?: boolean }) {
   // TODO(kristiehuang): AccountDialog UI does not yet exist
@@ -22,12 +23,24 @@ export default function ConnectedWalletChip({ disabled }: { disabled?: boolean }
   const [hover, setHover] = useState(false)
 
   const { account } = useWeb3React()
+  function disconnectWallet() {
+    connections.forEach(([wallet, _]) => {
+      if (!(wallet instanceof Network || wallet instanceof Url)) {
+        // Deactivate non-network wallet connector
+        if (wallet.deactivate) {
+          wallet.deactivate()
+        } else {
+          wallet.resetState()
+        }
+      }
+    })
+  }
 
   return (
     <>
       <AccountButton
         hidden={disabled}
-        // onClick={() => console.log('open account modal')}
+        onClick={() => hover && disconnectWallet()}
         color="secondary"
         data-testid="wallet"
         onMouseEnter={() => setHover(true)}
@@ -41,12 +54,12 @@ export default function ConnectedWalletChip({ disabled }: { disabled?: boolean }
             </Row>
           </ThemedText.Caption>
         ) : (
-          <Row gap={0.5}>
-            <Identicon />
-            <ThemedText.Subhead2>
+          <ThemedText.Subhead2>
+            <Row gap={0.5}>
+              <Identicon />
               {account?.substring(0, 6)}...{account?.substring(account.length - 4)}
-            </ThemedText.Subhead2>
-          </Row>
+            </Row>
+          </ThemedText.Subhead2>
         )}
       </AccountButton>
       {/* {open && (
