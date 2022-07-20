1fe2b66ff179315c6abfa0c68df20b281583a8a8
Kristie Huang <kristiehuang01@gmail.com>
2022-07-20 14:23:31 -0400

    feat: add disconnect hover

--- src/components/ConnectWallet/ConnectedWalletChip.tsx
+++ src/components/ConnectWallet/ConnectedWalletChip.tsx
@@ -1,18 +1,25 @@
+import { Trans } from '@lingui/macro'
+import { Wallet as WalletIcon } from 'icons'
 import { useWeb3React } from '@web3-react/core'
 import { TextButton } from 'components/Button'
 import Row from 'components/Row'
 import Identicon from 'icons/identicon'
 import styled from 'styled-components/macro'
 import { ThemedText } from 'theme'
+import { useState } from 'react'
 
 const AccountButton = styled(TextButton)<{ hidden?: boolean }>`
   filter: none;
   visibility: ${({ hidden }) => hidden && 'hidden'};
 `
 
+const AccountButtonText = styled(ThemedText.Subhead2)``
+
 export default function ConnectedWalletChip({ disabled }: { disabled?: boolean }) {
   // TODO(kristiehuang): AccountDialog UI does not yet exist
   // const [open, setOpen] = useState(false)
+
+  const [hover, setHover] = useState(false)
 
   const { account } = useWeb3React()
 
@@ -23,13 +30,24 @@ export default function ConnectedWalletChip({ disabled }: { disabled?: boolean }
         // onClick={() => console.log('open account modal')}
         color="secondary"
         data-testid="wallet"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       >
-        <ThemedText.Subhead2>
+        {hover ? (
+          <ThemedText.Caption>
+            <Row gap={0.5}>
+              <WalletIcon />
+              <Trans>Disconnect wallet</Trans>
+            </Row>
+          </ThemedText.Caption>
+        ) : (
           <Row gap={0.5}>
             <Identicon />
-            {account?.substring(0, 6)}...{account?.substring(account.length - 4)}
+            <ThemedText.Subhead2>
+              {account?.substring(0, 6)}...{account?.substring(account.length - 4)}
+            </ThemedText.Subhead2>
           </Row>
-        </ThemedText.Subhead2>
+        )}
       </AccountButton>
       {/* {open && (
         <Dialog color="module" onClose={() => setOpen(false)}>
