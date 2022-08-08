bd9fa0d9804db6818d7f6c63046a89e7d7264e48
Kristie Huang <kristiehuang01@gmail.com>
2022-08-08 16:51:59 -0700

    fix: Infura endpoints don't support requests from non-allowlisted origins; can't check eth_chainId to add new chain

--- src/hooks/connectWeb3/useWeb3React.tsx
+++ src/hooks/connectWeb3/useWeb3React.tsx
@@ -66,6 +66,32 @@ function getConnectionFromWalletConnect(
 }
 
 let jsonRpcUrlMap: { [chainId: number]: string[] }
+function getRpcUrls(chainId: SupportedChainId): string[] {
+  switch (chainId) {
+    case SupportedChainId.MAINNET:
+    case SupportedChainId.RINKEBY:
+    case SupportedChainId.ROPSTEN:
+    case SupportedChainId.KOVAN:
+    case SupportedChainId.GOERLI:
+      return jsonRpcUrlMap[chainId]
+    case SupportedChainId.OPTIMISM:
+      return ['https://mainnet.optimism.io']
+    case SupportedChainId.OPTIMISTIC_KOVAN:
+      return ['https://kovan.optimism.io']
+    case SupportedChainId.ARBITRUM_ONE:
+      return ['https://arb1.arbitrum.io/rpc']
+    case SupportedChainId.ARBITRUM_RINKEBY:
+      return ['https://rinkeby.arbitrum.io/rpc']
+    case SupportedChainId.POLYGON:
+      return ['https://polygon-rpc.com/']
+    case SupportedChainId.POLYGON_MUMBAI:
+      return ['https://rpc-endpoints.superfluid.dev/mumbai']
+    default:
+  }
+  // Our API-keyed URLs will fail security checks when used with external wallets.
+  throw new Error('RPC URLs must use public endpoints')
+}
+
 export const switchChain = async (connector: Connector, chainId: SupportedChainId) => {
   if (!ALL_SUPPORTED_CHAIN_IDS.includes(chainId)) {
     throw new Error(`Chain ${chainId} not supported`)
@@ -76,7 +102,7 @@ export const switchChain = async (connector: Connector, chainId: SupportedChainI
     const addChainParameter = {
       chainId,
       chainName: info.label,
-      rpcUrls: jsonRpcUrlMap[chainId],
+      rpcUrls: getRpcUrls(chainId),
       nativeCurrency: info.nativeCurrency,
       blockExplorerUrls: [info.explorer],
     }
