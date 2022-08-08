40df16961471ae695d56fb2ef632e249984b5ab1
Kristie Huang <kristiehuang01@gmail.com>
2022-08-08 13:29:22 -0700

    fix: make header text clickable

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -8,7 +8,7 @@ import styled from 'styled-components/macro'
 import { Color, Layer, ThemedText, ThemeProvider } from 'theme'
 import { delayUnmountForAnimation } from 'utils/animations'
 
-import { IconButton } from './Button'
+import { TextButton } from './Button'
 import Column from './Column'
 import Row from './Row'
 import Rule from './Rule'
@@ -59,6 +59,7 @@ const HeaderRow = styled(Row)`
   padding-top: 0.5em;
   ${largeIconCss}
 `
+
 interface HeaderProps {
   title?: ReactElement
   ruled?: boolean
@@ -70,10 +71,12 @@ export function Header({ title, children, ruled }: HeaderProps) {
     <>
       <Column>
         <HeaderRow iconSize={1.2}>
-          <Row justify="flex-start" gap={0.5}>
-            <IconButton color="primary" onClick={useContext(OnCloseContext)} icon={ChevronLeft} />
-            <Row gap={0.5}>{title && <ThemedText.Subhead1>{title}</ThemedText.Subhead1>}</Row>
-          </Row>
+          <TextButton color="primary" onClick={useContext(OnCloseContext)}>
+            <Row justify="flex-start" gap={0.5}>
+              <ChevronLeft />
+              <Row gap={0.5}>{title && <ThemedText.Subhead1>{title}</ThemedText.Subhead1>}</Row>
+            </Row>
+          </TextButton>
           {children}
         </HeaderRow>
         {ruled && <Rule padded />}
