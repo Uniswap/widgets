36472b9185301f6630f7cb753350793fb010a489
Connor McEwen <connor.mcewen@gmail.com>
2022-08-10 14:56:25 -0400

    set esm to true

--- scripts/compileAjvValidators.js
+++ scripts/compileAjvValidators.js
@@ -5,13 +5,13 @@ const standaloneCode = require('ajv/dist/standalone').default
 const addFormats = require('ajv-formats')
 const schema = require('@uniswap/token-lists/dist/tokenlist.schema.json')
 
-const ajv = new Ajv({ code: { source: true } })
+const ajv = new Ajv({ code: { source: true, esm: true } })
 addFormats(ajv)
 const validate = ajv.compile(schema)
 let moduleCode = standaloneCode(ajv, validate)
 fs.writeFileSync(path.join(__dirname, '../src/__generated__/validateTokenList.js'), moduleCode)
 
-const ajv2 = new Ajv({ code: { source: true } })
+const ajv2 = new Ajv({ code: { source: true, esm: true } })
 addFormats(ajv2)
 const validate2 = ajv2.compile({ ...schema, required: ['tokens'] })
 let moduleCode2 = standaloneCode(ajv2, validate2)
