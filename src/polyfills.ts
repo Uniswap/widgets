ead40ad51b04f4967725833b135655c785a33cac
Alex.Karys <alex.karys@UNISWAP-MAC-011.local>
2022-08-16 00:50:54 -0400

    Catching window error on server rener

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,8 @@
 import { Buffer } from 'buffer'
-
-// WalletConnect relies on Buffer, so it must be polyfilled.
-if (!('Buffer' in window)) {
-  window.Buffer = Buffer
+if (typeof window !== "undefined") {
+  // WalletConnect relies on Buffer, so it must be polyfilled.
+  if (!('Buffer' in window)) {
+    window.Buffer = Buffer
+  }
 }
+
