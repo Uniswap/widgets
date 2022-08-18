a3244348feff57b8357b8a528e4114a1b4e95375
Zach Pomerantz <zzmp@uniswap.org>
2022-08-18 15:34:08 -0700

    feat: call controlled handlers

--- src/hooks/swap/index.ts
+++ src/hooks/swap/index.ts
@@ -1,9 +1,9 @@
-import { Currency, CurrencyAmount } from '@uniswap/sdk-core'
+import { Currency, CurrencyAmount, TradeType } from '@uniswap/sdk-core'
 import { useAtom } from 'jotai'
 import { useAtomValue, useUpdateAtom } from 'jotai/utils'
 import { useCallback, useMemo } from 'react'
 import { pickAtom } from 'state/atoms'
-import { Field, swapAtom } from 'state/swap'
+import { controlledAtom, Field, swapAtom } from 'state/swap'
 import tryParseCurrencyAmount from 'utils/tryParseCurrencyAmount'
 export { default as useSwapInfo } from './useSwapInfo'
 
@@ -19,39 +19,54 @@ function otherField(field: Field) {
 }
 
 export function useSwitchSwapCurrencies() {
-  const update = useUpdateAtom(swapAtom)
+  const onSwitchTokens = useAtomValue(controlledAtom)?.onSwitchTokens
+  const setSwap = useUpdateAtom(swapAtom)
   return useCallback(() => {
-    update((swap) => {
-      const oldOutput = swap[Field.OUTPUT]
-      swap[Field.OUTPUT] = swap[Field.INPUT]
-      swap[Field.INPUT] = oldOutput
-      switch (swap.independentField) {
-        case Field.INPUT:
-          swap.independentField = Field.OUTPUT
-          break
-        case Field.OUTPUT:
-          swap.independentField = Field.INPUT
-          break
+    setSwap((swap) => {
+      if (onSwitchTokens) {
+        onSwitchTokens({
+          type: swap.independentField === Field.INPUT ? TradeType.EXACT_OUTPUT : TradeType.EXACT_INPUT,
+          amount: swap.amount,
+          inputToken: swap[Field.OUTPUT],
+          outputToken: swap[Field.INPUT],
+        })
+      } else {
+        const oldOutput = swap[Field.OUTPUT]
+        swap[Field.OUTPUT] = swap[Field.INPUT]
+        swap[Field.INPUT] = oldOutput
+        switch (swap.independentField) {
+          case Field.INPUT:
+            swap.independentField = Field.OUTPUT
+            break
+          case Field.OUTPUT:
+            swap.independentField = Field.INPUT
+            break
+        }
       }
     })
-  }, [update])
+  }, [onSwitchTokens, setSwap])
 }
 
-export function useSwapCurrency(field: Field): [Currency | undefined, (currency?: Currency) => void] {
-  const atom = useMemo(() => pickAtom(swapAtom, field), [field])
-  const otherAtom = useMemo(() => pickAtom(swapAtom, otherField(field)), [field])
-  const [currency, setCurrency] = useAtom(atom)
-  const otherCurrency = useAtomValue(otherAtom)
+export function useSwapCurrency(field: Field): [Currency | undefined, (currency: Currency) => void] {
+  const currencyAtom = useMemo(() => pickAtom(swapAtom, field), [field])
+  const [currency, setCurrency] = useAtom(currencyAtom)
+  const otherCurrencyAtom = useMemo(() => pickAtom(swapAtom, otherField(field)), [field])
+  const otherCurrency = useAtomValue(otherCurrencyAtom)
+  const onTokenChange = useAtomValue(controlledAtom)?.onTokenChange
   const switchSwapCurrencies = useSwitchSwapCurrencies()
   const setOrSwitchCurrency = useCallback(
-    (currency?: Currency) => {
+    (currency: Currency) => {
       if (currency === otherCurrency) {
         switchSwapCurrencies()
       } else {
-        setCurrency(currency)
+        if (onTokenChange) {
+          onTokenChange(field, currency)
+        } else {
+          setCurrency(currency)
+        }
       }
     },
-    [otherCurrency, setCurrency, switchSwapCurrencies]
+    [field, onTokenChange, otherCurrency, setCurrency, switchSwapCurrencies]
   )
   return [currency, setOrSwitchCurrency]
 }
@@ -74,14 +89,21 @@ export function useSwapAmount(field: Field): [string | undefined, (amount: strin
   const amount = useAtomValue(amountAtom)
   const isFieldIndependent = useIsSwapFieldIndependent(field)
   const value = isFieldIndependent ? amount : undefined
-  const updateSwap = useUpdateAtom(swapAtom)
+
+  const onAmountChange = useAtomValue(controlledAtom)?.onAmountChange
+  const setSwap = useUpdateAtom(swapAtom)
   const updateAmount = useCallback(
-    (amount: string) =>
-      updateSwap((swap) => {
-        swap.independentField = field
-        swap.amount = amount
-      }),
-    [field, updateSwap]
+    (amount: string) => {
+      if (onAmountChange) {
+        onAmountChange(field, amount)
+      } else {
+        setSwap((swap) => {
+          swap.independentField = field
+          swap.amount = amount
+        })
+      }
+    },
+    [field, onAmountChange, setSwap]
   )
   return [value, updateAmount]
 }
