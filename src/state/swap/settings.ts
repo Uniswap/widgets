72a8664a7f1b1d8bc3d7ea3e28874ac428c0c74e
Zach Pomerantz <zzmp@uniswap.org>
2022-08-19 15:38:16 -0700

    chore: rm unused client side router

--- src/state/swap/settings.ts
+++ src/state/swap/settings.ts
@@ -6,18 +6,15 @@ interface Settings {
   autoSlippage: boolean // if true, slippage will use the default calculation
   maxSlippage: number | undefined // expressed as a percent
   transactionTtl: number | undefined
-  clientSideRouter: boolean // whether to use the client-side router or query the remote API
 }
 
 const initialSettings: Settings = {
   autoSlippage: true,
   maxSlippage: undefined,
   transactionTtl: undefined,
-  clientSideRouter: false,
 }
 
 export const settingsAtom = atomWithReset(initialSettings)
 export const autoSlippageAtom = pickAtom(settingsAtom, 'autoSlippage')
 export const maxSlippageAtom = pickAtom(settingsAtom, 'maxSlippage')
 export const transactionTtlAtom = pickAtom(settingsAtom, 'transactionTtl')
-export const clientSideRouterAtom = pickAtom(settingsAtom, 'clientSideRouter')
