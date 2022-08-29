ea8231f1f1a69212c722ae2a42c6f0af95fc0170
Connor McEwen <connor.mcewen@gmail.com>
2022-08-29 13:25:01 -0400

    move generated + lib files

--- src/hooks/useArgentWalletContract.ts
+++ src/hooks/useArgentWalletContract.ts
@@ -1,6 +1,6 @@
 import { useWeb3React } from '@web3-react/core'
-import ArgentWalletContractABI from 'abis/argent-wallet-contract.json'
-import { ArgentWalletContract } from 'abis/types'
+import ArgentWalletContractABI from 'lib/abis/argent-wallet-contract.json'
+import { ArgentWalletContract } from 'lib/abis/types'
 
 import { useContract } from './useContract'
 import useIsArgentWallet from './useIsArgentWallet'
