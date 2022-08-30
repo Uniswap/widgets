28b3df4b16bb5dbd3731c4ddc566b5e5d96f97c8
Lint Action <lint-action@uniswap.org>
2022-08-30 15:14:41 +0000

    style(lint): lint action with ESLint

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,19 @@ import 'polyfills'
 import Swap, { SwapProps } from 'components/Swap'
 import Widget, { WidgetProps } from 'components/Widget'
 export type { Provider as EthersProvider } from '@ethersproject/abstract-provider'
-export { TradeType } from '@uniswap/sdk-core'
 export type { Currency } from '@uniswap/sdk-core'
+export { TradeType } from '@uniswap/sdk-core'
 export type { TokenInfo } from '@uniswap/token-lists'
 export type { Provider as Eip1193Provider } from '@web3-react/types'
 export type { ErrorHandler } from 'components/Error/ErrorBoundary'
 export { SupportedChainId } from 'constants/chains'
-export { DEFAULT_LOCALE, SUPPORTED_LOCALES } from 'constants/locales'
 export type { SupportedLocale } from 'constants/locales'
+export { DEFAULT_LOCALE, SUPPORTED_LOCALES } from 'constants/locales'
 export type { SwapController, SwapSettingsController } from 'hooks/swap/useSyncController'
 export type { FeeOptions } from 'hooks/swap/useSyncConvenienceFee'
 export type { DefaultAddress, TokenDefaults } from 'hooks/swap/useSyncTokenDefaults'
 export type { OnTxFail, OnTxSubmit, OnTxSuccess, TransactionEventHandlers } from 'hooks/transactions'
 export type { OnConnectWalletClick, WidgetEventHandlers } from 'hooks/useSyncWidgetEventHandlers'
-export { Field } from 'state/swap'
 export type {
   OnAmountChange,
   OnReviewSwapClick,
@@ -28,6 +27,7 @@ export type {
   OnTransactionDeadlineChange,
   SwapEventHandlers,
 } from 'state/swap'
+export { Field } from 'state/swap'
 export type { Slippage } from 'state/swap/settings'
 export type {
   ApprovalTransactionInfo,
@@ -40,8 +40,8 @@ export type {
   UnwrapTransactionInfo,
   WrapTransactionInfo,
 } from 'state/transactions'
-export { darkTheme, defaultTheme, lightTheme } from 'theme'
 export type { Theme } from 'theme'
+export { darkTheme, defaultTheme, lightTheme } from 'theme'
 export { invertTradeType, toTradeType } from 'utils/tradeType'
 
 export type SwapWidgetProps = SwapProps & WidgetProps
