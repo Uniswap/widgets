99e05d7c5cb20f99fc01cc680a746a7ddca92493
Alex Ball <alexball@UNISWAP-MAC-038.local>
2022-08-31 12:56:10 -0400

    removing dash

--- src/components/Swap/Input.tsx
+++ src/components/Swap/Input.tsx
@@ -121,7 +121,7 @@ export default function Input({ disabled, focused }: InputProps) {
       >
         <ThemedText.Body2 color="secondary" userSelect>
           <Row>
-            <USDC isLoading={isRouteLoading}>{usdc ? `$${formatCurrencyAmount(usdc, 6, 'en', 2)}` : '-'}</USDC>
+            <USDC isLoading={isRouteLoading}>{usdc ? `$${formatCurrencyAmount(usdc, 6, 'en', 2)}` : ''}</USDC>
             {balance && (
               <Balance color={balanceColor} focused={focused}>
                 Balance: <span>{formatCurrencyAmount(balance, 4, i18n.locale)}</span>
