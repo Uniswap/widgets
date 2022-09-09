23fc91060ed9738e7a0887e7769b948d99319db9
Zach Pomerantz <zzmp@uniswap.org>
2022-09-09 10:04:14 -0700

    refactor: simplify add/switch chain impls

--- src/hooks/useSwitchChain.ts
+++ src/hooks/useSwitchChain.ts
@@ -1,7 +1,6 @@
 import type { Web3Provider } from '@ethersproject/providers'
 import { useWeb3React } from '@web3-react/core'
 import { Network } from '@web3-react/network'
-import { ProviderRpcError } from '@web3-react/types'
 import { WalletConnect } from '@web3-react/walletconnect'
 import { getChainInfo } from 'constants/chainInfo'
 import { SupportedChainId } from 'constants/chains'
@@ -10,42 +9,46 @@ import useJsonRpcUrlMap from 'hooks/web3/useJsonRpcUrlMap'
 import { useCallback } from 'react'
 import invariant from 'tiny-invariant'
 
-async function switchChain(provider: Web3Provider, chainIdDecimal: SupportedChainId, rpcUrls: string[]): Promise<void> {
-  const { label: chainName, nativeCurrency, explorer } = getChainInfo(chainIdDecimal)
-  const chainId = `0x${chainIdDecimal.toString(16)}`
+function toHex(chainId: SupportedChainId): string {
+  return `0x${chainId.toString(16)}`
+}
 
-  try {
-    // EIP-3326 (used by MetaMask)
-    await provider.send('wallet_switchEthereumChain', [{ chainId }]).catch(async (error: ProviderRpcError) => {
-      if (error.code !== ErrorCode.CHAIN_NOT_ADDED) throw error
-      await addChain(rpcUrls)
-      await provider.send('wallet_switchEthereumChain', [{ chainId }])
+async function addChain(provider: Web3Provider, chainId: SupportedChainId, rpcUrls: string[]): Promise<void> {
+  const { label: chainName, nativeCurrency, explorer } = getChainInfo(chainId)
+  const addChainParameter = {
+    chainId: toHex(chainId),
+    chainName,
+    nativeCurrency,
+    blockExplorerUrls: [explorer],
+  }
 
-      async function addChain(rpcUrls: string[]) {
-        const addChainParameter = {
-          chainId,
-          chainName,
-          rpcUrls: [rpcUrls[0]],
-          nativeCurrency,
-          blockExplorerUrls: [explorer],
-        }
-        try {
-          // EIP-3085
-          await provider.send('wallet_addEthereumChain', [addChainParameter])
-        } catch (error) {
-          // Some providers (eg MetaMask) make test calls from a background page before switching,
-          // so fallback urls which are publicly available must be used. Otherwise, the switch will fail
-          // if the background page origin is blocked.
-          if (error?.code !== ErrorCode.USER_REJECTED_REQUEST && rpcUrls.length > 1) {
-            await addChain(rpcUrls.slice(1))
-          }
-        }
-      }
-    })
-  } catch (error) {
-    if (error?.code !== ErrorCode.USER_REJECTED_REQUEST) {
-      throw new Error(`Failed to switch network: ${error}`)
+  for (const rpcUrl of rpcUrls) {
+    try {
+      await provider.send('wallet_addEthereumChain', [{ ...addChainParameter, rpcUrls: [rpcUrl] }]) // EIP-3085
+    } catch (error) {
+      // Some providers (eg MetaMask) make test calls from a background page before switching,
+      // so fallback urls which are publicly available must be used. Otherwise, the switch will fail
+      // if the background page origin is blocked.
+      if (error?.code !== ErrorCode.USER_REJECTED_REQUEST) continue
+      throw error
     }
+  }
+}
+
+async function switchChain(provider: Web3Provider, chainId: SupportedChainId, rpcUrls: string[] = []): Promise<void> {
+  try {
+    try {
+      await provider.send('wallet_switchEthereumChain', [{ chainId: toHex(chainId) }]) // EIP-3326 (used by MetaMask)
+    } catch (error) {
+      if (error?.code === ErrorCode.CHAIN_NOT_ADDED && rpcUrls.length) {
+        await addChain(provider, chainId, rpcUrls)
+        return switchChain(provider, chainId)
+      }
+      throw error
+    }
+  } catch (error) {
+    if (error?.code === ErrorCode.USER_REJECTED_REQUEST) return
+    throw new Error(`Failed to switch network: ${error}`)
   }
 }
 
