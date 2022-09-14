9766757c13fc26e9fde7711e9f7da46963518c2a
Zach Pomerantz <zzmp@uniswap.org>
2022-09-14 16:38:23 -0700

    fix: guard against undefined window for SSR (eg nextjs) (#208)
    * Catching window error on server rener
    
    * style(lint): lint action with ESLint
    
    Co-authored-by: Alex.Karys <alex.karys@UNISWAP-MAC-011.local>
    Co-authored-by: Lint Action <lint-action@uniswap.org>

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,7 @@
 import { Buffer } from 'buffer'
-
-// WalletConnect relies on Buffer, so it must be polyfilled.
-if (!('Buffer' in window)) {
-  window.Buffer = Buffer
+if (typeof window !== 'undefined') {
+  // WalletConnect relies on Buffer, so it must be polyfilled.
+  if (!('Buffer' in window)) {
+    window.Buffer = Buffer
+  }
 }
