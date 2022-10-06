06b9ace41fad43a2f658bb41e54030879b98e3d2
Zach Pomerantz <zzmp@uniswap.org>
2022-10-05 19:23:05 -0700

    feat: consolidate GetQuoteArgs (#231)
    * build: upgrade smart-order-router
    
    * build: rm outdated v2-periphery
    
    * feat: consolidate GetQuoteArgs
    
    * build: trigger checks

--- src/state/routing/slice.ts
+++ src/state/routing/slice.ts
@@ -1,56 +1,17 @@
-import { JsonRpcProvider } from '@ethersproject/providers'
-import { isPlainObject } from '@reduxjs/toolkit'
-import { BaseQueryFn, createApi, skipToken } from '@reduxjs/toolkit/query/react'
+import { BaseQueryFn, createApi, SkipToken, skipToken } from '@reduxjs/toolkit/query/react'
 import { Protocol } from '@uniswap/router-sdk'
-import { TradeType } from '@uniswap/sdk-core'
-import type { ChainId } from '@uniswap/smart-order-router'
 import ms from 'ms.macro'
 import qs from 'qs'
+import { isExactInput } from 'utils/tradeType'
 
-import { GetQuoteResult } from './types'
+import { serializeGetQuoteArgs } from './args'
+import { GetQuoteArgs, GetQuoteResult } from './types'
 
 const protocols: Protocol[] = [Protocol.V2, Protocol.V3]
 
 // routing API quote query params: https://github.com/Uniswap/routing-api/blob/main/lib/handlers/quote/schema/quote-schema.ts
 const DEFAULT_QUERY_PARAMS = {
   protocols: protocols.map((p) => p.toLowerCase()).join(','),
-}
-
-export interface GetQuoteArgs {
-  tokenInAddress: string
-  tokenInChainId: ChainId
-  tokenInDecimals: number
-  tokenInSymbol?: string
-  tokenOutAddress: string
-  tokenOutChainId: ChainId
-  tokenOutDecimals: number
-  tokenOutSymbol?: string
-  amount: string
-  routerUrl?: string
-  tradeType: TradeType
-  provider: JsonRpcProvider
-}
-
-const NON_SERIALIZABLE_KEYS = ['provider']
-
-/** Omits the non-serializable keys from GetQuoteArgs' cache key. */
-function serializeGetQuoteArgs({ endpointName, queryArgs }: { endpointName: string; queryArgs: GetQuoteArgs }) {
-  // same as default serializeQueryArgs, but ignoring non-serializable keys.
-  return `${endpointName}(${JSON.stringify(queryArgs, (key, value) => {
-    if (NON_SERIALIZABLE_KEYS.includes(key)) {
-      return undefined
-    }
-    if (isPlainObject(value)) {
-      return Object.keys(value)
-        .sort()
-        .reduce<any>((acc, key) => {
-          acc[key] = (value as any)[key]
-          return acc
-        }, {})
-    } else {
-      return value
-    }
-  })})`
 }
 
 const baseQuery: BaseQueryFn<GetQuoteArgs, GetQuoteResult> = () => {
@@ -63,30 +24,14 @@ export const routing = createApi({
   serializeQueryArgs: serializeGetQuoteArgs,
   endpoints: (build) => ({
     getQuote: build.query({
-      async queryFn(args) {
+      async queryFn(args: GetQuoteArgs | SkipToken) {
         if (args === skipToken) return { error: { status: 'CUSTOM_ERROR', error: 'Skipped' } }
 
-        const {
-          tokenInAddress,
-          tokenInChainId,
-          tokenOutAddress,
-          tokenOutChainId,
-          amount,
-          routerUrl,
-          tradeType,
-          provider,
-        } = args
-
-        async function getClientSideQuote() {
-          // Lazy-load the client-side router to improve initial pageload times.
-          return await (
-            await import('../../hooks/routing/clientSideSmartOrderRouter')
-          ).getClientSideQuote(args, provider, { protocols })
-        }
-
         // If enabled, try routing API, falling back to clientside SOR.
-        if (Boolean(routerUrl)) {
+        if (Boolean(args.routerUrl)) {
           try {
+            const { tokenInAddress, tokenInChainId, tokenOutAddress, tokenOutChainId, amount, tradeType } = args
+            const type = isExactInput(tradeType) ? 'exactIn' : 'exactOut'
             const query = qs.stringify({
               ...DEFAULT_QUERY_PARAMS,
               tokenInAddress,
@@ -94,9 +39,9 @@ export const routing = createApi({
               tokenOutAddress,
               tokenOutChainId,
               amount,
-              type: tradeType === TradeType.EXACT_INPUT ? 'exactIn' : 'exactOut',
+              type,
             })
-            const response = await global.fetch(`${routerUrl}quote?${query}`)
+            const response = await global.fetch(`${args.routerUrl}quote?${query}`)
             if (!response.ok) {
               return { error: { status: response.status, data: await response.text() } }
             }
@@ -110,7 +55,9 @@ export const routing = createApi({
 
         // If integrator did not provide a routing API URL param, use clientside SOR
         try {
-          const result = await getClientSideQuote()
+          // Lazy-load the client-side router to improve initial pageload times.
+          const clientSideSmartOrderRouter = await import('../../hooks/routing/clientSideSmartOrderRouter')
+          const result = await clientSideSmartOrderRouter.getClientSideQuote(args, { protocols })
           return result
         } catch (error) {
           console.warn(`GetQuote failed on client: ${error}`)
