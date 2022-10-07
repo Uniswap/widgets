3cc6d928bfc60891499af98075b7cbd89ddbff9a
Lynn Yu <lynn.yu@uniswap.org>
2022-10-07 12:22:50 -0400

    delete

--- src/components/Swap/Summary/Summary.tsx
+++ src/components/Swap/Summary/Summary.tsx
@@ -6,7 +6,6 @@ import { PropsWithChildren } from 'react'
 import styled from 'styled-components/macro'
 import { ThemedText } from 'theme'
 import { formatCurrencyAmount } from 'utils/formatCurrencyAmount'
-import { formatPercentage } from 'utils/formatPercentage'
 
 import Column from '../../Column'
 import Row from '../../Row'
@@ -59,7 +58,7 @@ export default function Summary({ input, output, inputUSDC, outputUSDC, impact, 
       <TokenValue input={input} usdc={inputUSDC} open={open} />
       {open ? <ArrowRight /> : <ArrowDown />}
       <TokenValue input={output} usdc={outputUSDC} open={open}>
-        {impact && <ThemedText.Caption color={impact.warning}>({formatPercentage(impact.percent)})</ThemedText.Caption>}
+        {impact && <ThemedText.Caption color={impact.warning}>({impact.toString()})</ThemedText.Caption>}
       </TokenValue>
     </>
   )
--- src/utils/formatPercentage.ts
+++
@@ -1,9 +0,0 @@
-import { Percent } from '@uniswap/sdk-core'
-
-export function formatPercentage(percentage: Percent): string {
-  const exactFloat = Number(percentage.numerator) / Number(percentage.denominator)
-  if (exactFloat < 0.005) {
-    return '0.00%'
-  }
-  return `${exactFloat.toFixed(2)}%`
-}
