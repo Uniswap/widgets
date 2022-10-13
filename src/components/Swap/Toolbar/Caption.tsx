f00cd089ff11b5e5a01f022aaf1d2ccb2f9ab890
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2022-10-12 21:59:37 -0500

    fix: center button and toolbar text with relevant icons (#264)
    * center button and toolbar text with relevant icons
    
    * pr feedback

--- src/components/Swap/Toolbar/Caption.tsx
+++ src/components/Swap/Toolbar/Caption.tsx
@@ -28,7 +28,9 @@ function Caption({ icon: Icon = AlertTriangle, caption }: CaptionProps) {
   return (
     <>
       <Icon color="secondary" />
-      <ThemedText.Caption color="secondary">{caption}</ThemedText.Caption>
+      <ThemedText.Caption color="secondary" lineHeight="14px">
+        {caption}
+      </ThemedText.Caption>
     </>
   )
 }
