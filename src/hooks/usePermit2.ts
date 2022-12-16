dea62d9c688326a03ead63c99ee56c22b30cb5df
Zach Pomerantz <zzmp@uniswap.org>
2022-12-16 11:13:48 -0800

    fix: simplify permit2 logic

--- src/hooks/usePermit2.ts
+++ src/hooks/usePermit2.ts
@@ -10,115 +10,114 @@ import useInterval from './useInterval'
 import { PermitSignature, usePermitAllowance, useUpdatePermitAllowance } from './usePermitAllowance'
 import { useTokenAllowance, useUpdateTokenAllowance } from './useTokenAllowance'
 
-enum SyncState {
+enum ApprovalState {
   PENDING,
   SYNCING,
   SYNCED,
 }
 
-export enum PermitState {
-  INVALID,
-  LOADING,
-  APPROVAL_OR_PERMIT_NEEDED,
-  APPROVAL_LOADING,
-  APPROVED_AND_PERMITTED,
+export enum AllowanceState {
+  UNKNOWN,
+  REQUIRED,
+  ALLOWED,
 }
 
-export interface Permit {
-  token?: Token
-  state: PermitState
-  signature?: PermitSignature
-  callback?: () => Promise<ApprovalTransactionInfo | void>
+export interface AllowanceRequired {
+  state: AllowanceState.REQUIRED
+  token: Token
+  isApprovalLoading: boolean
+  callback: () => Promise<ApprovalTransactionInfo | void>
 }
 
-export default function usePermit(amount?: CurrencyAmount<Token>, spender?: string): Permit {
+export type Allowance =
+  | { state: AllowanceState.UNKNOWN }
+  | {
+      state: AllowanceState.ALLOWED
+      signature?: PermitSignature
+    }
+  | AllowanceRequired
+
+export default function usePermit2Allowance(amount?: CurrencyAmount<Token>, spender?: string): Allowance {
   const { account } = useWeb3React()
   const token = amount?.currency
+
   const { tokenAllowance, isSyncing: isApprovalSyncing } = useTokenAllowance(token, account, PERMIT2_ADDRESS)
   const updateTokenAllowance = useUpdateTokenAllowance(amount, PERMIT2_ADDRESS)
-  const isAllowed = useMemo(
-    () => amount && (tokenAllowance?.greaterThan(amount) || tokenAllowance?.equalTo(amount)),
-    [amount, tokenAllowance]
-  )
+  const isApproved = useMemo(() => {
+    if (!amount || !tokenAllowance) return false
+    return tokenAllowance.greaterThan(amount) || tokenAllowance.equalTo(amount)
+  }, [amount, tokenAllowance])
 
-  const permitAllowance = usePermitAllowance(token, spender)
-  const [permitAllowanceAmount, setPermitAllowanceAmount] = useState(permitAllowance?.amount)
-  useEffect(() => setPermitAllowanceAmount(permitAllowance?.amount), [permitAllowance?.amount])
-  const isPermitted = useMemo(
-    () => amount && permitAllowanceAmount?.gte(amount.quotient.toString()),
-    [amount, permitAllowanceAmount]
-  )
-
-  const [signature, setSignature] = useState<PermitSignature>()
-  const updatePermitAllowance = useUpdatePermitAllowance(token, spender, permitAllowance?.nonce, setSignature)
-  const isSigned = useMemo(
-    () => amount && signature?.details.token === token?.address && signature?.spender === spender,
-    [amount, signature?.details.token, signature?.spender, spender, token?.address]
-  )
-
-  // Trigger a re-render if either tokenAllowance or signature expire.
-  useInterval(
-    () => {
-      // Calculate now such that the signature will still be valid for the next block.
-      const now = (Date.now() - STANDARD_L1_BLOCK_TIME) / 1000
-      if (signature && signature.sigDeadline < now) {
-        setSignature(undefined)
-      }
-      if (permitAllowance && permitAllowance.expiration < now) {
-        setPermitAllowanceAmount(undefined)
-      }
-    },
-    STANDARD_L1_BLOCK_TIME,
-    true
-  )
-
-  // Permit2 should be marked syncing from the time approval is submitted (pending) until it is
-  // synced in tokenAllowance, to avoid re-prompting the user for an already-submitted approval.
-  const [syncState, setSyncState] = useState(SyncState.SYNCED)
-  const isApprovalLoading = syncState !== SyncState.SYNCED
-  const hasPendingApproval = Boolean(usePendingApproval(token, PERMIT2_ADDRESS))
+  // Marks approval as loading from the time it is submitted (pending), until it has confirmed and another block synced.
+  // This avoids re-prompting the user for an already-submitted but not-yet-observed approval, by marking it loading
+  // until it has been re-observed. It wll sync immediately, because confirmation fast-forwards the block number.
+  const [approvalState, setApprovalState] = useState(ApprovalState.SYNCED)
+  const isApprovalLoading = approvalState !== ApprovalState.SYNCED
+  const isApprovalPending = Boolean(usePendingApproval(token, PERMIT2_ADDRESS))
   useEffect(() => {
-    if (hasPendingApproval) {
-      setSyncState(SyncState.PENDING)
+    if (isApprovalPending) {
+      setApprovalState(ApprovalState.PENDING)
     } else {
-      setSyncState((state) => {
-        if (state === SyncState.PENDING && isApprovalSyncing) {
-          return SyncState.SYNCING
-        } else if (state === SyncState.SYNCING && !isApprovalSyncing) {
-          return SyncState.SYNCED
-        } else {
-          return state
+      setApprovalState((state) => {
+        if (state === ApprovalState.PENDING && isApprovalSyncing) {
+          return ApprovalState.SYNCING
+        } else if (state === ApprovalState.SYNCING && !isApprovalSyncing) {
+          return ApprovalState.SYNCED
         }
+        return state
       })
     }
-  }, [hasPendingApproval, isApprovalSyncing])
+  }, [isApprovalPending, isApprovalSyncing])
+
+  // Signature and PermitAllowance will expire, so they should be rechecked at an interval.
+  const [now, setNow] = useState(Date.now())
+  // Calculate now such that the signature will still be valid for the submitting block.
+  useInterval(() => setNow((Date.now() - STANDARD_L1_BLOCK_TIME) / 1000), STANDARD_L1_BLOCK_TIME, true)
+
+  const [signature, setSignature] = useState<PermitSignature>()
+  const isSigned = useMemo(() => {
+    if (!amount || !signature) return false
+    return signature.details.token === token?.address && signature.spender === spender && signature.sigDeadline >= now
+  }, [amount, now, signature, spender, token?.address])
+
+  const { permitAllowance, expiration: permitExpiration, nonce } = usePermitAllowance(token, account, spender)
+  const updatePermitAllowance = useUpdatePermitAllowance(token, spender, nonce, setSignature)
+  const isPermitted = useMemo(() => {
+    if (!amount || !permitAllowance || !permitExpiration) return false
+    return (permitAllowance.greaterThan(amount) || permitAllowance.equalTo(amount)) && permitExpiration >= now
+  }, [amount, now, permitAllowance, permitExpiration])
 
   const callback = useCallback(async () => {
     let info: ApprovalTransactionInfo | undefined
-    if (!isAllowed && !hasPendingApproval) {
+    if (!(isApproved || isApprovalLoading)) {
       info = await updateTokenAllowance()
     }
-    if (!isPermitted && !isSigned) {
+    if (!(isPermitted || isSigned)) {
       await updatePermitAllowance()
     }
     return info
-  }, [hasPendingApproval, isAllowed, isPermitted, isSigned, updatePermitAllowance, updateTokenAllowance])
+  }, [isApprovalLoading, isApproved, isPermitted, isSigned, updatePermitAllowance, updateTokenAllowance])
 
   return useMemo(() => {
-    if (!token) {
-      return { state: PermitState.INVALID }
-    } else if (!tokenAllowance || !permitAllowance) {
-      return { token, state: PermitState.LOADING }
-    } else if (!(isPermitted || isSigned)) {
-      return { token, state: PermitState.APPROVAL_OR_PERMIT_NEEDED, callback }
-    } else if (!isAllowed) {
-      return {
-        token,
-        state: isApprovalLoading ? PermitState.APPROVAL_LOADING : PermitState.APPROVAL_OR_PERMIT_NEEDED,
-        callback,
+    if (token) {
+      if (!tokenAllowance || !permitAllowance) {
+        return { state: AllowanceState.UNKNOWN }
+      } else if (!(isPermitted || isSigned)) {
+        return { token, state: AllowanceState.REQUIRED, isApprovalLoading: false, callback }
+      } else if (!isApproved) {
+        return { token, state: AllowanceState.REQUIRED, isApprovalLoading, callback }
       }
-    } else {
-      return { token, state: PermitState.APPROVED_AND_PERMITTED, signature: isPermitted ? undefined : signature }
     }
-  }, [callback, isAllowed, isApprovalLoading, isPermitted, isSigned, permitAllowance, signature, token, tokenAllowance])
+    return { token, state: AllowanceState.ALLOWED, signature: !isPermitted && isSigned ? signature : undefined }
+  }, [
+    callback,
+    isApprovalLoading,
+    isApproved,
+    isPermitted,
+    isSigned,
+    permitAllowance,
+    signature,
+    token,
+    tokenAllowance,
+  ])
 }
