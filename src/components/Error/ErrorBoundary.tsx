258f4350d0c323268c0a3ce9c23a257717f93d2c
eddie <66155195+just-toby@users.noreply.github.com>
2023-01-04 14:47:23 -0800

    feat: create new ErrorContext for errors that are not thrown (#345)
    * feat: create new ErrorContext for errors that are not thrown
    
    * refactor: simplify error context usage
    
    * feat: add useSetError to set async errors for the boundary
    
    * fix: define error state in ctor
    
    * fix: better field init in WidgetError
    
    Co-authored-by: Zach Pomerantz <zzmp@uniswap.org>

--- src/components/Error/ErrorBoundary.tsx
+++ src/components/Error/ErrorBoundary.tsx
@@ -1,46 +1,58 @@
-import { Trans } from '@lingui/macro'
-import { Component, ErrorInfo, PropsWithChildren } from 'react'
+import { DEFAULT_ERROR_ACTION, DEFAULT_ERROR_HEADER, WidgetError } from 'errors'
+import { Component, createContext, ErrorInfo, PropsWithChildren, useContext } from 'react'
 
 import Dialog from '../Dialog'
 import ErrorDialog from './ErrorDialog'
 
-export type OnError = (error: Error, info: ErrorInfo) => void
+const ErrorContext = createContext<{ setError: (e: WidgetError) => void }>({ setError: () => undefined })
+
+export type OnError = (error: Error, info?: ErrorInfo) => void
 
 interface ErrorBoundaryProps {
   onError?: OnError
 }
 
 type ErrorBoundaryState = {
-  error: Error | null
+  error?: Error
+}
+
+export function useSetError() {
+  return useContext(ErrorContext).setError
 }
 
 export default class ErrorBoundary extends Component<PropsWithChildren<ErrorBoundaryProps>, ErrorBoundaryState> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props)
-    this.state = { error: null }
-  }
-
   static getDerivedStateFromError(error: Error) {
     return { error }
+  }
+
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = {}
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     this.props.onError?.(error, errorInfo)
   }
 
+  renderErrorDialog(error: Error) {
+    const header = error instanceof WidgetError ? error.header : DEFAULT_ERROR_HEADER
+    const action = error instanceof WidgetError ? error.action : DEFAULT_ERROR_ACTION
+    return (
+      <Dialog color="dialog">
+        <ErrorDialog error={error} header={header} action={action} onClick={() => window.location.reload()} />
+      </Dialog>
+    )
+  }
+
+  setError(error: WidgetError) {
+    this.props.onError?.(error)
+    this.setState({ error })
+  }
+
   render() {
     if (this.state.error) {
-      return (
-        <Dialog color="dialog">
-          <ErrorDialog
-            error={this.state.error}
-            header={<Trans>Please refresh the page and try again.</Trans>}
-            action={<Trans>Reload the page</Trans>}
-            onClick={() => window.location.reload()}
-          />
-        </Dialog>
-      )
+      return this.renderErrorDialog(this.state.error)
     }
-    return this.props.children
+    return <ErrorContext.Provider value={{ setError: this.setError }}>{this.props.children}</ErrorContext.Provider>
   }
 }
--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,34 @@
-export class IntegrationError extends Error {
+import { t } from '@lingui/macro'
+
+export const DEFAULT_ERROR_HEADER = t`Please refresh the page and try again.`
+export const DEFAULT_ERROR_ACTION = t`Reload the page`
+
+export abstract class WidgetError extends Error {
+  header: string
+  action: string
+
+  constructor(config: { header?: string; action?: string; message?: string }) {
+    super(config.message)
+    this.header = config.header ?? DEFAULT_ERROR_HEADER
+    this.action = config.action ?? DEFAULT_ERROR_ACTION
+  }
+}
+
+export class IntegrationError extends WidgetError {
   constructor(message: string) {
-    super(message)
+    super({ message })
     this.name = 'Integration Error'
+  }
+}
+
+class ConnectionError extends WidgetError {}
+
+export class MetaMaskConnectionError extends ConnectionError {
+  constructor() {
+    super({
+      header: t`Wallet disconnected`,
+      action: t`Reload`,
+      message: t`'A Metamask error caused your wallet to disconnect. Reload the page to reconnect.'`,
+    })
   }
 }
