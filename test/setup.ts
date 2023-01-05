d662f0516c043b30de1a614fd05ac9a6ef6a9508
Eddie Dugan <eddie.dugan@uniswap.org>
2023-01-04 16:11:41 -0800

    fix: rename ResizeObserver

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,4 @@
-import ResizeObserverPolyfill from 'resize-observer-polyfill'
 import { TextDecoder, TextEncoder } from 'util'
 
 global.TextDecoder = TextDecoder as typeof global.TextDecoder
 global.TextEncoder = TextEncoder as typeof global.TextEncoder
-global.ResizeObserver = ResizeObserverPolyfill
