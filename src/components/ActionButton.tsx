38a8d91addfe62a9625ccf10c1227eb1efcce00c
Zach Pomerantz <zzmp@uniswap.org>
2023-01-10 01:57:37 -0800

    test: improve ApproveButton testing

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -99,7 +99,7 @@ export default function ActionButton({
     }
   }, [color])
   return (
-    <Overlay hasAction={Boolean(action)} flex align="stretch" {...wrapperProps}>
+    <Overlay data-testid="action-button" hasAction={Boolean(action)} flex align="stretch" {...wrapperProps}>
       <StyledButton color={color} disabled={disabled} onClick={action?.onClick || onClick} {...rest}>
         <ThemedText.TransitionButton buttonSize={action ? 'medium' : 'large'} color={textColor}>
           {action?.children || children}
