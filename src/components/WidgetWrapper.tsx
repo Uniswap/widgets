4a15878471c3f90ff950e13c25ea5743adeb95cc
cartcrom <cartergcromer@gmail.com>
2023-01-10 17:08:32 -0500

    refactor: removed redundant wrapped css

--- src/components/WidgetWrapper.tsx
+++ src/components/WidgetWrapper.tsx
@@ -7,12 +7,8 @@ const StyledWidgetWrapper = styled.div`
   border: ${({ theme }) => `1px solid ${theme.outline}`};
   border-radius: ${({ theme }) => theme.borderRadius}em;
   box-shadow: ${({ theme }) => `0px 40px 120px 0px ${theme.networkDefaultShadow}`};
-  box-sizing: border-box;
   color: ${({ theme }) => theme.primary};
-  display: flex;
-  flex-direction: column;
   padding: ${HORIZONTAL_PADDING}px;
-  position: relative;
 `
 
 export default StyledWidgetWrapper
