a0de2d631938ac32e0bfce0e901e48dd955cc51b
Eddie Dugan <eddie.dugan@uniswap.org>
2023-01-12 14:36:58 -0800

    feat: update colors and icon in token select dialog

--- src/components/TokenSelect/index.tsx
+++ src/components/TokenSelect/index.tsx
@@ -1,10 +1,12 @@
 import { t, Trans } from '@lingui/macro'
 import { Currency } from '@uniswap/sdk-core'
 import { useWeb3React } from '@web3-react/core'
+import { StringInput } from 'components/Input'
 import { useConditionalHandler } from 'hooks/useConditionalHandler'
 import { useCurrencyBalances } from 'hooks/useCurrencyBalance'
 import useNativeCurrency from 'hooks/useNativeCurrency'
 import useTokenList, { useIsTokenListLoaded, useQueryTokens } from 'hooks/useTokenList'
+import { Search } from 'icons'
 import { useAtomValue } from 'jotai/utils'
 import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Field, swapEventHandlersAtom } from 'state/swap'
@@ -13,7 +15,6 @@ import { ThemedText } from 'theme'
 
 import Column from '../Column'
 import Dialog, { Header } from '../Dialog'
-import { inputCss, StringInput } from '../Input'
 import Row from '../Row'
 import Rule from '../Rule'
 import NoTokensAvailableOnNetwork from './NoTokensAvailableOnNetwork'
@@ -21,8 +22,20 @@ import TokenButton from './TokenButton'
 import TokenOptions, { TokenOptionsHandle } from './TokenOptions'
 import TokenOptionsSkeleton from './TokenOptionsSkeleton'
 
-const SearchInput = styled(StringInput)`
-  ${inputCss}
+const SearchInputContainer = styled(Row)`
+  background-color: ${({ theme }) => theme.module};
+  border: 1px solid ${({ theme }) => theme.outline};
+  border-radius: ${({ theme }) => theme.borderRadius}em;
+  justify-content: flex-start;
+  padding: calc(0.75em - 1px);
+
+  &:focus-within {
+    border-color: ${({ theme }) => theme.accentSoft};
+  }
+`
+
+const StyledSearchIcon = styled(Search)`
+  color: ${({ theme }) => theme.secondary};
 `
 
 function usePrefetchBalances() {
@@ -78,26 +91,29 @@ export function TokenSelectDialog({ value, onSelect, onClose }: TokenSelectDialo
 
   if (!listHasTokens && isLoaded) {
     return (
-      <Dialog color="module" onClose={onClose}>
+      <Dialog color="container" onClose={onClose}>
         <Header title={<Trans>Select a token</Trans>} />
         <NoTokensAvailableOnNetwork />
       </Dialog>
     )
   }
   return (
-    <Dialog color="module" onClose={onClose}>
+    <Dialog color="container" onClose={onClose}>
       <Header title={<Trans>Select a token</Trans>} />
       <Column gap={0.75}>
         <Row pad={0.75} grow>
-          <ThemedText.Body1>
-            <SearchInput
-              value={query}
-              onChange={setQuery}
-              placeholder={t`Search by token name or address`}
-              onKeyDown={options?.onKeyDown}
-              ref={input}
-            />
-          </ThemedText.Body1>
+          <SearchInputContainer gap={0.75} justify="start" flex>
+            <StyledSearchIcon />
+            <ThemedText.Body1 flexGrow={1}>
+              <StringInput
+                value={query}
+                onChange={setQuery}
+                placeholder={t`Search by token name or address`}
+                onKeyDown={options?.onKeyDown}
+                ref={input}
+              />
+            </ThemedText.Body1>
+          </SearchInputContainer>
         </Row>
         <Rule padded />
       </Column>
