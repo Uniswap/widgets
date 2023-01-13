0db1abe390e1bb4b6b5041c859130eb2db826056
Eddie Dugan <eddie.dugan@uniswap.org>
2023-01-13 10:25:04 -0800

    fix: polish design issues in widget

--- src/components/Swap/Settings/TransactionTtlInput.tsx
+++ src/components/Swap/Settings/TransactionTtlInput.tsx
@@ -11,6 +11,7 @@ import { Label } from './components'
 
 const Input = styled(Row)`
   ${inputCss}
+  border: 1px solid ${({ theme }) => theme.outline};
 `
 
 export default function TransactionTtlInput() {
