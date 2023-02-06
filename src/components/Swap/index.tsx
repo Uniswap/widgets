116764fe96717846f65046485d57cb14009a5057
eddie <66155195+just-toby@users.noreply.github.com>
2023-02-06 09:51:39 -0800

    fix: hide connection UI based on integration prop (#443)

--- src/components/Swap/index.tsx
+++ src/components/Swap/index.tsx
@@ -64,7 +64,7 @@ export default function Swap(props: SwapProps) {
             <Output />
             <ToolbarProvider>
               {account && <Toolbar />}
-              <SwapActionButton />
+              <SwapActionButton hideConnectionUI={props.hideConnectionUI} />
             </ToolbarProvider>
             {useBrandedFooter() && <BrandedFooter />}
           </SwapInfoProvider>
