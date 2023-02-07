10321cfee7599c69bbc286fcf3774bde2f4423b6
Daniel Constantin <hello@danielconstantin.net>
2023-02-07 19:50:47 +0200

    Remove fixture options

--- src/cosmos/Swap.fixture.tsx
+++ src/cosmos/Swap.fixture.tsx
@@ -1,12 +1,9 @@
-import { darkTheme, DEFAULT_LOCALE, defaultTheme, lightTheme, SUPPORTED_LOCALES, SwapWidget } from '@uniswap/widgets'
+import { darkTheme, defaultTheme, lightTheme, SwapWidget } from '@uniswap/widgets'
 import Row from 'components/Row'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useValue } from 'react-cosmos/fixture'
 
-import { DAI, USDC_MAINNET } from '../constants/tokens'
 import EventFeed, { Event, HANDLERS } from './EventFeed'
-import useOption from './useOption'
-import useProvider from './useProvider'
 
 function Fixture() {
   const [events, setEvents] = useState<Event[]>([])
@@ -18,22 +15,22 @@ function Fixture() {
   )
 
   // TODO(zzmp): Changing defaults has no effect if done after the first render.
-  const currencies: Record<string, string> = {
-    Native: 'NATIVE',
-    DAI: DAI.address,
-    USDC: USDC_MAINNET.address,
-  }
-  const defaultInputToken = useOption('defaultInputToken', { options: currencies })
-  const [defaultInputAmount] = useValue('defaultInputAmount', { defaultValue: 0 })
-  const defaultOutputToken = useOption('defaultOutputToken', { options: currencies })
-  const [defaultOutputAmount] = useValue('defaultOutputAmount', { defaultValue: 0 })
+  // const currencies: Record<string, string> = {
+  //   Native: 'NATIVE',
+  //   DAI: DAI.address,
+  //   USDC: USDC_MAINNET.address,
+  // }
+  // const defaultInputToken = useOption('defaultInputToken', { options: currencies })
+  // const [defaultInputAmount] = useValue('defaultInputAmount', { defaultValue: 0 })
+  // const defaultOutputToken = useOption('defaultOutputToken', { options: currencies })
+  // const [defaultOutputAmount] = useValue('defaultOutputAmount', { defaultValue: 0 })
 
-  const [hideConnectionUI] = useValue('hideConnectionUI', { defaultValue: false })
+  // const [hideConnectionUI] = useValue('hideConnectionUI', { defaultValue: false })
 
   const [width] = useValue('width', { defaultValue: 420 })
 
-  const locales = [...SUPPORTED_LOCALES, 'fa-KE (unsupported)', 'pseudo']
-  const locale = useOption('locale', { options: locales, defaultValue: DEFAULT_LOCALE, nullable: false })
+  // const locales = [...SUPPORTED_LOCALES, 'fa-KE (unsupported)', 'pseudo']
+  // const locale = useOption('locale', { options: locales, defaultValue: DEFAULT_LOCALE, nullable: false })
 
   const [theme, setTheme] = useValue('theme', { defaultValue: defaultTheme })
   const [darkMode] = useValue('darkMode', { defaultValue: false })
@@ -45,7 +42,7 @@ function Fixture() {
   // })
   // const defaultChainId = defaultNetwork ? CHAIN_NAMES_TO_IDS[defaultNetwork] : undefined
 
-  const connector = useProvider()
+  // const connector = useProvider()
 
   const eventHandlers = useMemo(
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -60,9 +57,9 @@ function Fixture() {
       // defaultOutputTokenAddress={defaultOutputToken}
       // defaultOutputAmount={defaultOutputAmount}
       // hideConnectionUI={hideConnectionUI} // TODO(Daniel) remove
-      locale={locale}
+      // locale={locale}
       // defaultChainId={defaultChainId} // TODO(Daniel) remove
-      provider={connector} // TODO(Daniel) remove
+      // provider={connector} // TODO(Daniel) remove
       theme={theme}
       // tokenList={tokenList} // TODO(Daniel) remove
       width={width}
