ab48ee0c9767bb302425e8d576f9a4a83a3721e4
Eddie Dugan <eddie.dugan@uniswap.org>
2023-02-27 17:42:32 -0800

    fix: lint

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -117,7 +117,7 @@ export default function ActionButton({
     }
   }, [color, disabled])
 
-  const buttonSize = useMemo(() => narrow ? 'small' : action ? 'medium' : 'large', [narrow, action])
+  const buttonSize = useMemo(() => (narrow ? 'small' : action ? 'medium' : 'large'), [narrow, action])
 
   return (
     <Overlay
