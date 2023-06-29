4ac3338371ddb8cea3cdef09bd49aa6bf003f9b9
Jordan Frankfurt <jordan@CORN-Jordan-949.frankfurt>
2023-06-29 10:20:21 -0400

    feat(wallet-connect): add support for v2

--- src/utils/WalletConnect.ts
+++ src/utils/WalletConnect.ts
@@ -1,45 +1,41 @@
-import 'setimmediate'
+import { WalletConnect, WalletConnectConstructorArgs } from '@web3-react/walletconnect-v2'
+import { L1_CHAIN_IDS, L2_CHAIN_IDS, SupportedChainId } from 'constants/chains'
 
-import { URI_AVAILABLE, WalletConnect, WalletConnectConstructorArgs } from '@web3-react/walletconnect'
-import QRCode from 'qrcode'
+import { JSON_RPC_FALLBACK_ENDPOINTS } from 'constants/jsonRpcEndpoints'
 
-export class WalletConnectPopup extends WalletConnect {
-  constructor({ actions, options, defaultChainId, timeout, onError }: WalletConnectConstructorArgs) {
-    super({ actions, options: { ...options, qrcode: true }, defaultChainId, timeout, onError })
-  }
-}
 
-export class WalletConnectQR extends WalletConnect {
-  static SVG_AVAILABLE = 'svg_available'
+// Avoid testing for the best URL by only passing a single URL per chain.
+// Otherwise, WC will not initialize until all URLs have been tested (see getBestUrl in web3-react).
+const RPC_URLS_WITHOUT_FALLBACKS = Object.entries(JSON_RPC_FALLBACK_ENDPOINTS).reduce(
+  (map, [chainId, urls]) => ({
+    ...map,
+    [chainId]: urls[0],
+  }),
+  {}
+)
+const optionalChains = [...L1_CHAIN_IDS, ...L2_CHAIN_IDS].filter((x) => x !== SupportedChainId.MAINNET)
 
-  svg?: string
-
-  constructor({ actions, options, defaultChainId, timeout, onError }: WalletConnectConstructorArgs) {
-    super({ actions, options: { ...options, qrcode: false }, defaultChainId, timeout, onError })
-
-    this.events.once(URI_AVAILABLE, () => {
-      this.provider?.connector.on('disconnect', () => {
-        this.deactivate()
-      })
+export class WalletConnectV2 extends WalletConnect {
+  ANALYTICS_EVENT = 'Wallet Connect QR Scan'
+  constructor({
+    actions,
+    onError,
+    qrcode = true,
+  }: Omit<WalletConnectConstructorArgs, 'options'> & { qrcode?: boolean }) {
+    super({
+      actions,
+      options: {
+        projectId: process.env.REACT_APP_WALLET_CONNECT_PROJECT_ID as string,
+        optionalChains,
+        chains: [SupportedChainId.MAINNET],
+        showQrModal: qrcode,
+        rpcMap: RPC_URLS_WITHOUT_FALLBACKS,
+        // as of 6/16/2023 there are no docs for `optionalMethods`
+        // this set of optional methods fixes a bug we encountered where permit2 signatures were never received from the connected wallet
+        // source: https://uniswapteam.slack.com/archives/C03R5G8T8BH/p1686858618164089?thread_ts=1686778867.145689&cid=C03R5G8T8BH
+        optionalMethods: ['eth_signTypedData', 'eth_signTypedData_v4', 'eth_sign'],
+      },
+      onError,
     })
-
-    this.events.on(URI_AVAILABLE, async (uri) => {
-      this.svg = undefined
-      if (!uri) return
-
-      this.svg = await QRCode.toString(uri, {
-        // Leave a margin to increase contrast in dark mode.
-        margin: 1,
-        // Use 55*2=110 for the width to prevent distortion. The generated viewbox is "0 0 55 55".
-        width: 110,
-        type: 'svg',
-      })
-      this.events.emit(WalletConnectQR.SVG_AVAILABLE, this.svg)
-    })
-  }
-
-  deactivate() {
-    this.events.emit(URI_AVAILABLE)
-    return super.deactivate()
   }
 }
